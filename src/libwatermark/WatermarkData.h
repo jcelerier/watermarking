f1b88388eeae1d4ed63a0b038ed4a609c6e14318
Jean-Michaël Celerier <jcelerier@enseirb-matmeca.fr>
2013-11-25 11:42:57 +0100

    bits à quentin

--- src/libwatermark/WatermarkData.h
+++ src/libwatermark/WatermarkData.h
@@ -1,12 +1,27 @@
 #pragma once
 #include <vector>
+#include <stdint.h>
 
 class WatermarkData
 {
 	public:
+		uint64_t _size;
 		std::vector<bool> bits;
 		unsigned int position = 0;
 		WatermarkData() = default;
+
+		void setSize(uint64_t size)
+		{
+			_size = size;
+			for(int index = 0; index<sizeof(size); index++)
+			{
+				uint64_t bit64 = 0x00000001;
+				bool bit;
+				bit = size & (0x000000001 >> index);
+				bits.push_back(bit);
+				position++;
+			}
+		}
 };
 
 

0b9adb5ef4ce68c1c7ffa5cc79d9170bcfac0156
ademartin <ademartin@enseirb-matmeca.fr>
2013-11-25 11:56:45 +0100

    JM é vréman tro bô(bis)

--- src/libwatermark/watermark/SSWEncode.h
+++ src/libwatermark/watermark/SSWEncode.h
@@ -2,6 +2,7 @@
 #define SSWENCODE_H
 
 #include <cstdlib>
+#include <vector>
 
 #include "SpectralWatermarkBase.h"
 
@@ -20,10 +21,10 @@ class SSWEncode : public SpectralWatermarkBase<data_type>
         virtual void operator()(Audio_p& data, WatermarkData& watermark)  override
         {
 
-            /*
             // Recopier cette ligne
             auto& spectrum = static_cast<CData<typename SpectralWatermarkBase<data_type>::complex_type>*>(data.get())->_data;
 
+            /*
             // Petit exemple qui va multiplier l'amplitude de chaque bande de spectre par _gain.
             for(auto& channel : spectrum)
             {
@@ -41,6 +42,43 @@ class SSWEncode : public SpectralWatermarkBase<data_type>
             }
             */
 
+        }
+
+        /* Génère une séquence pseudo-aléatoire à valeurs comprises dans {-1,1}
+            de taille size */
+        std::vector<int> generatePNSequence(int size)
+        {
+            srand(time(0));
+            vector<int> pnsequence;
+
+            for(int i = 0; i < size; i++)
+            {
+                int j = rand()%2;
+                if(j == 0)
+                    pnsequence.push_back(-1);
+                else
+                    pnsequence.push_back(1);
+            }
+
+            return pnsequence;
+        }
+
+        /* Génère une plage de fréquence aléatoire de taille size et comprise dans
+            l'intervalle [1,sampleRate] */
+        std::vector<int> generateFrequencyRange(int size, int sampleRate)
+        {
+            srand(time(0));
+            vector<int> frequencyRange;
+
+            int j = (rand()%(sampleRate-size))+1;
+            frequencyRange.push_back(j);
+
+            for(int i = 1; i < size; i++)
+            {
+                frequencyRange.push_back(i+j);
+            }
+
+            return frequencyRange;
         }
 
         virtual void onFFTSizeUpdate() override

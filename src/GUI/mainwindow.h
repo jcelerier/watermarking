c2c244b0829abd19f10e32eea212455244a63581
ademartin <ademartin@enseirb-matmeca.fr>
2013-12-09 14:50:50 +0100

    Adding factice Qt Interface (minor modifications)
    Minor modifications ...

--- src/GUI/mainwindow.h
+++ src/GUI/mainwindow.h
@@ -1,20 +1,22 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include<QMainWindow>
+#include <QMainWindow>
 
-namespace Ui{
-  class MainWindow;
+namespace Ui {
+class MainWindow;
 }
 
-class MainWindow:public QMainWindow{
-  Q_OBJECT
-  public:
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    void caca();
-  private:
+
+private:
     Ui::MainWindow *ui;
 };
 
-#endif /*MAINWINDOW_H*/
+#endif // MAINWINDOW_H
